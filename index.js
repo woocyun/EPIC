18d361ff5c6eb13d26ac1dc3aaf03f2885aa2856
Woo Yun <woocyun@gmail.com>
2017-06-30 23:37:49 -0700

    create tmp directory, download earth images for date into tmp directory, once all images are downloaded, execute ffmpeg command to stitch images into a slideshow

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+const exec = require('child_process').exec;
 const moment = require('moment');
 
 const date = '2017-06-29';
@@ -8,6 +9,7 @@ const year = wrappedDate.format('YYYY');
 const month = wrappedDate.format('MM');
 const day = wrappedDate.format('DD');
 
+const apiKey = 'yJ7I1YmphfhICUaAKkXV7fsCEGGBIEIiGblhBv0g';
 const baseUrl = 'https://api.nasa.gov/EPIC';
 const outputType = 'jpg';
 
@@ -29,13 +31,36 @@ axios.get(`${ baseUrl }/api/natural/date/${ date }?api_key=yJ7I1YmphfhICUaAKkXV7
         return new Date(a.date) - new Date(b.date);
       });
 
+    const promises = [];
+
     timepoints.forEach((timepoint, index) => {
-      axios.get(
-        `${ baseUrl }/archive/natural/${ year }/${ month }/${ day }/${ outputType }/epic_1b_${ timepoint.identifier }.${ outputType }?api_key=yJ7I1YmphfhICUaAKkXV7fsCEGGBIEIiGblhBv0g`,
-        { responseType: 'stream' }
-      )
+      const promise = axios
+        .get(
+          `${ baseUrl }/archive/natural/${ year }/${ month }/${ day }/${ outputType }/epic_1b_${ timepoint.identifier }.${ outputType }?api_key=${ apiKey }`,
+          { responseType: 'stream' }
+        )
         .then(({ data }) => {
-          data.pipe(fs.createWriteStream(`./tmp/img${ index + 1 }.${ outputType }`));
+          return new Promise((resolve, reject) => {
+            const file = fs.createWriteStream(`./tmp/img${ index + 1 }.${ outputType }`);
+            data.pipe(file);
+            file.on('finish', () => {
+              resolve();
+            });
+          });
         });
+
+      promises.push(promise);
     });
+
+    Promise
+      .all(promises)
+      .then(response => {
+        exec(
+          `ffmpeg -framerate 4 -i ${ __dirname }/tmp/img%d.jpg -codec copy ${ __dirname }/${ date }.mkv`,
+          (error, stdout, stderror) => {
+            if (error) throw error;
+            console.log('finished!');
+          }
+        );
+      });
   });
