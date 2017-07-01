35d4e838fd2d776cee2633a5e01d07f963936a0d
Woo Yun <woocyun@gmail.com>
2017-06-30 22:27:44 -0700

    make tmp directory, loop over day's images

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const axios = require('axios');
+const fs = require('fs');
 const moment = require('moment');
 
-const date = '2016-07-14';
+const date = '2017-06-29';
 const wrappedDate = moment(date);
 const year = wrappedDate.format('YYYY');
 const month = wrappedDate.format('MM');
 const day = wrappedDate.format('DD');
 
-const baseUrl = 'https://epic.gsfc.nasa.gov';
+const baseUrl = 'https://api.nasa.gov/EPIC';
+const outputType = 'jpg';
 
-axios.get(`${ baseUrl }/api/enhanced/date/${ date }`)
+try {
+  fs.mkdirSync(__dirname + '/tmp');
+} catch (err) {
+  if (err.code !== 'EEXIST') throw err;
+}
+
+axios.get(`${ baseUrl }/api/natural/date/${ date }?api_key=yJ7I1YmphfhICUaAKkXV7fsCEGGBIEIiGblhBv0g`)
   .then(({ data }) => {
-    const asset = data[0];
-    const imageIdentifier = asset.image.split('_').slice(2).join('_');
-
-    axios.get(
-      `${ baseUrl }/archive/natural/${ year }/${ month }/${ day }/jpg/epic_1b_${ imageIdentifier }.jpg`,
-      { responseType: 'arraybuffer' }
-    )
-      .then(({ data }) => {
-        console.log(data);
+    const timepoints = data
+      .map(asset => {
+        return Object.assign({}, asset, {
+          identifier: asset.image.split('_').slice(2).join('_')
+        });
+      })
+      .sort((a, b) => {
+        return new Date(a.date) - new Date(b.date);
       });
+
+    timepoints.forEach((timepoint, index) => {
+      axios.get(
+        `${ baseUrl }/archive/natural/${ year }/${ month }/${ day }/${ outputType }/epic_1b_${ timepoint.identifier }.${ outputType }?api_key=yJ7I1YmphfhICUaAKkXV7fsCEGGBIEIiGblhBv0g`,
+        { responseType: 'stream' }
+      )
+        .then(({ data }) => {
+          data.pipe(fs.createWriteStream(`./tmp/img${ index + 1 }.${ outputType }`));
+        });
+    });
   });
